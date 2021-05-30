6322d81706a8657ea14ed5a353644580174eaf3e
Valmir Dimas <valmirdimas@gmail.com>
2021-05-30 19:05:03 -0300

    Remove ActiveRecord references

--- lib/rails/observers/railtie.rb
+++ lib/rails/observers/railtie.rb
@@ -3,14 +3,6 @@ require 'rails/railtie'
 module Rails
   module Observers
     class Railtie < ::Rails::Railtie
-      initializer "active_record.observer", :before => "active_record.set_configs" do |app|
-        ActiveSupport.on_load(:active_record) do
-          require "rails/observers/activerecord/active_record"
-          observers = app.config.active_record.delete(:observers)
-          self.observers = observers if observers
-        end
-      end
-
       initializer "action_controller.caching.sweepers" do
         ActiveSupport.on_load(:action_controller) do
           require "rails/observers/action_controller/caching"
@@ -36,16 +28,6 @@ module Rails
           # the class we're loading. 💣💥
           require "active_record/base" if defined?(ActiveRecord)
         rescue LoadError
-        end
-
-        ActiveSupport.on_load(:active_record) do
-          ActiveRecord::Base.instantiate_observers
-
-          # Rails 5.1 forward-compat. AD::R is deprecated to AS::R in Rails 5.
-          reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Reloader
-          reloader.to_prepare do
-            ActiveRecord::Base.instantiate_observers
-          end
         end
 
         ActiveSupport.on_load(:active_resource) do
